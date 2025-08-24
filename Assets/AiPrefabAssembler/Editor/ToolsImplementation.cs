d87e830ba61f5b1eb52a9b7e654fbd3d8e61afb8
Bert Haddad <bhaddad5@gmail.com>
2025-08-24 14:07:08 -0700

    Metadata fetch works, but doesn't ACTUALLY work :P

--- Assets/AiPrefabAssembler/Editor/ToolsImplementation.cs
+++ Assets/AiPrefabAssembler/Editor/ToolsImplementation.cs
@@ -9,39 +9,12 @@ public class ToolsImplementation : IToolsImplementation
 	public string GetPartMetadata(string part)
 	{
 		string prefabPath = $"{folder}/{part}.prefab";
-		GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-		if (prefab == null)
+		var comp = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath)?.GetComponent<AiMetadataFlag>();
+		if (comp == null)
 		{
 			return "";
 		}
 
-		var bounds = GetPrefabBounds(prefabPath);
-		if (bounds == null)
-			return "";
-
-		return $"[{part}, width:{bounds.Value.max.x - bounds.Value.min.x}, height:{bounds.Value.max.y - bounds.Value.min.y}, depth:{bounds.Value.max.z - bounds.Value.min.z}],";
-	}
-
-	private static Bounds? GetPrefabBounds(string prefabPath)
-	{
-		// Load prefab asset
-		GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-		if (prefab == null)
-		{
-			Debug.LogError($"Could not load prefab at path: {prefabPath}");
-			return null;
-		}
-
-		// Get the MeshCollider on the root
-		MeshCollider meshCollider = prefab.GetComponent<MeshCollider>();
-		if (meshCollider == null)
-		{
-			Debug.LogError("Prefab root does not have a MeshCollider.");
-			return null;
-		}
-
-		// Return collider bounds (local to prefab)
-		// In editor, collider.bounds is world-space, so we use sharedMesh.bounds
-		return meshCollider.sharedMesh != null ? meshCollider.sharedMesh.bounds : null;
+		return $"[{part}, metadata:{comp.AiMetadata}],";
 	}
 }
